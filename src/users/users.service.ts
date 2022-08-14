9e71909ea3fa0ec519e1ef253969f257f2f8e243
이승환 <lsh01087674117@gmail.com>
2022-08-14 16:58:42 +0900

    안쓰는 코드 정리

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,23 +4,11 @@ import {UpdateUserDto} from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
+  create(CreateUserDto: CreateUserDto) {
     return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }
