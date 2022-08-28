1b79203d60f9d649c94e4e56d5d6f4dcf4ba7fc3
이승환 <lsh01087674117@gmail.com>
2022-08-28 16:32:02 +0900

    메일옵션 타입에 주석처리

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -4,9 +4,9 @@ import * as nodemailer from 'nodemailer';
 import {Injectable} from '@nestjs/common';
 
 interface EmailOptions {
-  to: string;
-  subject: string;
-  html: string;
+  to: string; // 수신자
+  subject: string;  // 메일제목
+  html: string; // 메일본문
 }
 
 @Injectable()
