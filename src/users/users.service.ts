4675f3463243e3d64be0c30f771faa389d73e79d
이승환 <lsh01087674117@gmail.com>
2022-08-28 14:47:15 +0900

    가입하려는 유저가 존재여부, 유저저장, 회원가입 인증이메일 메서드 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,4 +9,16 @@ export class UsersService {
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
+
+  private checkUserExists(email: string) {
+    return false; // TODO: DB 연동 후 구현
+  }
+
+  private saveUser(name: string, email: string, password: string, signupVerifyToken: string) {
+    return; // TODO: DB 연동 후 구현
+  }
+
+  private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
+    await this.emailService.sendMemberJoinVerification(email, signupVerifyToken);
+  }
 }
