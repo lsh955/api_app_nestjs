f6adc9552714b4feb073be4ca24847ec0c43ac7c
이승환 <lsh01087674117@gmail.com>
2022-08-28 16:41:13 +0900

    메일전송 전문

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -25,4 +25,29 @@ export class EmailService {
       },
     });
   }
+
+  async sendMemberJoinVerification(
+    emailAddress: string,
+    signupVerifyToken: string,
+  ) {
+    const baseUrl = 'http://localhost:3000';
+
+    // 유저가 누를 버튼이 가질 링크를 구성, 이 링크로 다시 우리 서비스로 이메일 인증 요청이 들어온다.
+    const url = `${baseUrl}/users/email-verify?signupVerifyToken=${signupVerifyToken}`;
+
+    // 메일본문 구성, form 태그를 이용하여 POST 요청
+    const mailOptions: EmailOptions = {
+      to: emailAddress,
+      subject: '가입인증 메일 안내',
+      html: `
+        가입확인 버튼를 누르시면 가입 인증이 완료됩니다.<br/>
+        <form action="${url}" method="POST">
+          <button>가입확인</button>
+        </form>
+      `,
+    };
+
+    // transporter 객체를 이용하여 메일을 전송
+    return await this.transporter.sendMail(mailOptions);
+  }
 }
