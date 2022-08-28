6df55497131e4a749b725101d5fcf65ad8ee7170
이승환 <lsh01087674117@gmail.com>
2022-08-28 16:30:28 +0900

    메일 발신을 위한 관련 라이브러리 import

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,13 @@
+import Mail = require('nodemailer/lib/mailer');
+import * as nodemailer from 'nodemailer';
+
 import {Injectable} from '@nestjs/common';
+
+interface EmailOptions {
+  to: string;
+  subject: string;
+  html: string;
+}
 
 @Injectable()
 export class EmailService {}
