ff99185936db939b9a75359720b009dcce76582e
이승환 <lsh01087674117@gmail.com>
2022-08-28 16:22:39 +0900

    이메일 처리를 담당하는 이메일서비스 프로바이터 추가 및 import 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import {Module} from '@nestjs/common';
 import {UsersController} from './users/users.controller';
 import {UsersService} from './users/users.service';
+import {EmailService} from './email/email.service';
 
 @Module({
   imports: [],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, EmailService],
 })
 export class AppModule {}
