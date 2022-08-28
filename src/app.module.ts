b67f8770e2e166001769c8c328f2be39cd2fee63
이승환 <lsh01087674117@gmail.com>
2022-08-28 12:53:31 +0900

    users.module.ts 관리는 app.module.ts 으로 이관하여 관리에 따른 작업

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import {Module} from '@nestjs/common';
-import {UsersModule} from './users/users.module';
+import {UsersController} from './users/users.controller';
+import {UsersService} from './users/users.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class AppModule {}
