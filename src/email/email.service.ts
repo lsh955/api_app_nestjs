35299f0857acbbba4684fc2226da17db4d2f551f
이승환 <lsh01087674117@gmail.com>
2022-08-28 16:40:36 +0900

    주석정렬

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -4,9 +4,9 @@ import * as nodemailer from 'nodemailer';
 import {Injectable} from '@nestjs/common';
 
 interface EmailOptions {
-  to: string; // 수신자
+  to: string;       // 수신자
   subject: string;  // 메일제목
-  html: string; // 메일본문
+  html: string;     // 메일본문
 }
 
 @Injectable()
