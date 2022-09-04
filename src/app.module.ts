001f1f4c29a53c131da80c45d60f127d954eda2b
이승환 <lsh01087674117@gmail.com>
2022-09-04 19:24:59 +0900

    AppModule 에는 UsersModule 이 import되어 AppModule 에서 직접 UsersController, UsersService, EmailService 를 참조 할 필요가 없으므로 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import {Module} from '@nestjs/common';
-import {UsersController} from './users/users.controller';
-import {UsersService} from './users/users.service';
-import {EmailService} from './email/email.service';
+import {UsersModule} from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [UsersController],
-  providers: [UsersService, EmailService],
+  imports: [UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
