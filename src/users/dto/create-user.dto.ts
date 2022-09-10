efe37c3dfe562fe5a02340dc85e9fdf45dc4edfe
이승환 <lsh01087674117@gmail.com>
2022-09-10 15:20:25 +0900

    class-validator 를 이용하여 규칙적용

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,20 @@
+import {IsEmail, IsString, Matches, MaxLength, MinLength,} from 'class-validator';
+
 export class CreateUserDto {
+  // 사용자 이름은 2자 이상 30자 이하인 문자열
+  @IsString()
+  @MinLength(2)
+  @MaxLength(30)
   readonly name: string;
+
+  // 사용자 이메일은 60자 이하의 문자열로써 이메일 주소형식
+  @IsString()
+  @IsEmail()
+  @MaxLength(60)
   readonly email: string;
+
+  // 사용자 패스워드는 영문대소문자와 숫자 또는 특수문자(!, @, #, $, %, ^, &, *, (, ))로 이루어진 8자 이상 30자 이하의 문자열
+  @IsString()
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
   readonly password: string;
 }
