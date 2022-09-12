f876ad0aea9893db89a1ba1ca06f35b2d0d4e935
이승환 <lsh01087674117@gmail.com>
2022-09-12 13:57:10 +0900

    유저모듈 내에서 사용할 저장소를 등록

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,13 @@
 import {Module} from '@nestjs/common';
-import {EmailModule} from '../email/email.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+
 import {UsersController} from './users.controller';
+import {UserEntity} from './entities/user.entity';
+import {EmailModule} from '../email/email.module';
 import {UsersService} from './users.service';
 
 @Module({
-  imports: [EmailModule],
+  imports: [EmailModule, TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
   providers: [UsersService],
 })
