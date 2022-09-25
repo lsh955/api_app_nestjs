10ed4758902805467e024b1611fca51dfb074300
이승환 <lsh01087674117@gmail.com>
2022-09-25 14:58:32 +0900

    HttpExceptionFilter 에서 예외 처리 도중 콘솔에 로그를 처리하는 부분을 Logger 를 이용하도록 변경

--- src/exception/http-exception.filter.ts
+++ src/exception/http-exception.filter.ts
@@ -1,12 +1,22 @@
-import {ArgumentsHost, Catch, ExceptionFilter, HttpException, InternalServerErrorException,} from '@nestjs/common';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import {Request, Response} from 'express';
 
 @Catch() // 처리되지 않은 모든 예외를 잡으려고 할 때 사용
 export class HttpExceptionFilter implements ExceptionFilter {
+  constructor(private logger: Logger) {}
+
   catch(exception: Error, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const res = ctx.getResponse<Response>();
     const req = ctx.getRequest<Request>();
+    const stack = exception.stack;
 
     if (!(exception instanceof HttpException)) {
       exception = new InternalServerErrorException();
@@ -18,9 +28,10 @@ export class HttpExceptionFilter implements ExceptionFilter {
       timestamp: new Date(),
       url: req.url,
       response,
+      stack,
     };
 
-    console.log(log);
+    this.logger.log(log);
 
     res.status((exception as HttpException).getStatus()).json(response);
   }
