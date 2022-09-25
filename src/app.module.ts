7009770d35fcc9f838443410f15eaa43da8abbdd
이승환 <lsh01087674117@gmail.com>
2022-09-25 12:03:19 +0900

    전역적으로 authConfig 로드

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import {TypeOrmModule} from '@nestjs/typeorm';
 import {ConfigModule} from '@nestjs/config';
 
 import emailConfig from './config/emailConfig';
+import authConfig from './config/authConfig';
 import {validationSchema} from './config/validationSchema';
 import {UsersModule} from './users/users.module';
 
@@ -13,7 +14,7 @@ import {UsersModule} from './users/users.module';
       // out 디렉토리(dist 디렉토리) 아래에 존재하는 파일인 .XXX.env 파일의 절대 경로를 가리키게 설정
       envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
       // load 속성을 통해 앞에서 구성해둔 ConfigFactory 를 지정
-      load: [emailConfig],
+      load: [emailConfig, authConfig],
       // 전역 모듈로 동작하도록 하여 어느 모듈에서나 사용할 수 있도록 하기 위해
       isGlobal: true,
       // 환경변수의 값에 대해 유효성 검사를 수행하도록 joi 를 이용하여 유효성 검사 객체를 작성
