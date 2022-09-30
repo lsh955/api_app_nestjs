32b56d08dad756ea7ceb28cd415778bc6d29cd07
이승환 <lsh01087674117@gmail.com>
2022-09-30 14:55:00 +0900

    컨트롤러에 HTTP 헬스체크 코드를 구현

--- src/health-check/health-check.controller.ts
+++ src/health-check/health-check.controller.ts
@@ -1,4 +1,18 @@
-import {Controller} from '@nestjs/common';
+import {Controller, Get} from '@nestjs/common';
+import {HealthCheck, HealthCheckService, HttpHealthIndicator,} from '@nestjs/terminus';
 
 @Controller('health-check')
-export class HealthCheckController {}
+export class HealthCheckController {
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+    ]);
+  }
+}
