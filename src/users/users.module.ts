5c7528cc48d8b4666fda220aeb2198e90707bde4
이승환 <lsh01087674117@gmail.com>
2022-10-01 13:20:28 +0900

    CQRS 모듈을 사용하도록 UsersModule 에 import

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,6 @@
 import {Logger, Module} from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
+import {CqrsModule} from '@nestjs/cqrs';
 
 import {UsersController} from './users.controller';
 import {UserEntity} from './entities/user.entity';
@@ -8,7 +9,12 @@ import {AuthModule} from 'src/auth/auth.module';
 import {UsersService} from './users.service';
 
 @Module({
-  imports: [EmailModule, TypeOrmModule.forFeature([UserEntity]), AuthModule],
+  imports: [
+    EmailModule,
+    TypeOrmModule.forFeature([UserEntity]),
+    AuthModule,
+    CqrsModule,
+  ],
   controllers: [UsersController],
   providers: [UsersService, Logger],
 })
