fcf111f9acc4f37304ac9d64abb6509604ae4c26
이승환 <lsh01087674117@gmail.com>
2022-10-09 15:24:41 +0900

    리펙토링

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,16 +1,37 @@
 import {Logger, Module} from '@nestjs/common';
-import {TypeOrmModule} from '@nestjs/typeorm';
 import {CqrsModule} from '@nestjs/cqrs';
-
-import {UsersController} from './interface/users.controller';
-import {UserEntity} from './infra/db/entity/user.entity';
-import {EmailModule} from '../email/email.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
 import {AuthModule} from 'src/auth/auth.module';
+import {EmailModule} from 'src/email/email.module';
 import {CreateUserHandler} from './application/command/create-user.handler';
-import {UserEventsHandler} from './application/event/user-events.handler';
+import {LoginHandler} from './application/command/login.handler';
+import {VerifyAccessTokenHandler} from './application/command/verify-access-token.handler';
+import {VerifyEmailHandler} from './application/command/verify-email.handler';
 import {UserFactory} from './domain/user.factory';
+import {UserEntity} from './infra/db/entity/user.entity';
+import {UserEventsHandler} from './application/event/user-events.handler';
+import {GetUserInfoQueryHandler} from './application/query/get-user-info.handler';
+import {UsersController} from './interface/users.controller';
 import {UserRepository} from './infra/db/repository/UserRepository';
 import {EmailService} from './infra/adapter/email.service';
+
+const commandHandlers = [
+  CreateUserHandler,
+  VerifyEmailHandler,
+  LoginHandler,
+  VerifyAccessTokenHandler,
+];
+
+const queryHandlers = [GetUserInfoQueryHandler];
+
+const eventHandlers = [UserEventsHandler];
+
+const factories = [UserFactory];
+
+const repositories = [
+  {provide: 'UserRepository', useClass: UserRepository},
+  {provide: 'EmailService', useClass: EmailService},
+];
 
 @Module({
   imports: [
@@ -18,15 +39,15 @@ import {EmailService} from './infra/adapter/email.service';
     TypeOrmModule.forFeature([UserEntity]),
     AuthModule,
     CqrsModule,
-    UserFactory,
   ],
   controllers: [UsersController],
   providers: [
-    CreateUserHandler,
-    UserEventsHandler,
     Logger,
-    {provide: 'UserRepository', useClass: UserRepository},
-    {provide: 'EmailService', useClass: EmailService},
+    ...commandHandlers,
+    ...queryHandlers,
+    ...eventHandlers,
+    ...factories,
+    ...repositories,
   ],
 })
 export class UsersModule {}
