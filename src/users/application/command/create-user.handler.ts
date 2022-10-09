ce62b82809a57f5a6dff0d86e568f1e52025b51e
이승환 <lsh01087674117@gmail.com>
2022-10-09 14:47:07 +0900

    IUserRepository 를 이용하여 데이터를 다를 수 있게되어 CreateUserHandler 의 execute 리펙토링

--- src/users/application/command/create-user.handler.ts
+++ src/users/application/command/create-user.handler.ts
@@ -1,47 +1,41 @@
 import * as uuid from 'uuid';
-import {Injectable, UnprocessableEntityException} from '@nestjs/common';
-import {CommandHandler, EventBus, ICommandHandler} from '@nestjs/cqrs';
+import {ulid} from 'ulid';
+import {Inject, Injectable, UnprocessableEntityException} from '@nestjs/common';
+import {CommandHandler, ICommandHandler} from '@nestjs/cqrs';
 import {CreateUserCommand} from './create-user.command';
-import {InjectRepository} from '@nestjs/typeorm';
-import {UserEntity} from '../../infra/db/entity/user.entity';
-import {Connection, Repository} from 'typeorm';
-import {UserCreatedEvent} from '../../domain/user-created.event';
+import {UserFactory} from '../../domain/user.factory';
+import {IUserRepository} from 'src/users/domain/repository/iuser.repository';
 
 @Injectable()
 @CommandHandler(CreateUserCommand)
 export class CreateUserHandler implements ICommandHandler<CreateUserCommand> {
   constructor(
-    private connection: Connection,
-    private eventBus: EventBus,
-
-    @InjectRepository(UserEntity)
-    private usersRepository: Repository<UserEntity>,
+    private userFactory: UserFactory,
+    @Inject('UserRepository') private userRepository: IUserRepository,
   ) {}
 
   async execute(command: CreateUserCommand) {
     const {name, email, password} = command;
 
-    const userExist = await this.checkUserExists(email);
-    if (userExist) {
+    const user = await this.userRepository.findByEmail(email);
+    if (user !== null) {
       throw new UnprocessableEntityException(
         '해당 이메일로는 가입할 수 없습니다.',
       );
     }
 
+    const id = ulid();
     const signupVerifyToken = uuid.v1();
 
-    await this.saveUserUsingTransaction(
+    await this.userRepository.save(
+      id,
       name,
       email,
       password,
       signupVerifyToken,
     );
 
-    // FIXME :: 비즈니스 요구사항이 추가로 생겨 난다면, 또 다른 이벤트 핸들러에서 요구사항을 처리하는 로직을 구현할 것.
-
-    // 회원가입 이메일 전송 로직을 회원가입 이벤트를 통해 처리
-    // EmailService 를 이용하는 부분을 UserCreatedEvent 를 발송하도록 변경
-    this.eventBus.publish(new UserCreatedEvent(email, signupVerifyToken));
+    this.userFactory.create(id, name, email, password, signupVerifyToken);
   }
 
   /**
