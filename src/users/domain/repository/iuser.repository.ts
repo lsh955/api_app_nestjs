917f8c1fe7dd6f6e2f384c31027f36164a8157af
이승환 <lsh01087674117@gmail.com>
2022-10-09 14:46:19 +0900

    정렬

--- src/users/domain/repository/iuser.repository.ts
+++ src/users/domain/repository/iuser.repository.ts
@@ -3,9 +3,9 @@ import {User} from '../user';
 export interface IUserRepository {
   findByEmail: (email: string) => Promise<User>;
   save: (
-    name: string,
-    email: string,
-    password: string,
-    signupVerifyToken: string,
-  ) => Promise<void>;
+      name: string,
+      email: string,
+      password: string,
+      signupVerifyToken: string,
+      signupVerifyToken1: any) => Promise<void>;
 }
