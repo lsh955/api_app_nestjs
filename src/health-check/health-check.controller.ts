15d59e1e3505d07dc0f07ad826a659d186bee8f6
이승환 <30866509+lsh955@users.noreply.github.com>
2022-10-24 08:53:37 +0900

    pingCheck 응답값 수정

--- src/health-check/health-check.controller.ts
+++ src/health-check/health-check.controller.ts
@@ -14,8 +14,8 @@ export class HealthCheckController {
   check() {
     return this.health.check([
       // FIXME :: pingCheck 함수를 이용해서 서비스가 제공하는 다른 서버가 잘 동작하고 있는지 확인
-      // URL 요청을 보내서 응답을 잘 받으면 응답 결과에 첫번째 인자로 넣은 nestjs-docs 로 응답
-      () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+      //  URL 요청을 보내서 응답을 잘 받으면 응답 결과에 첫번째 인자로 넣은 docs 로 응답
+      () => this.http.pingCheck('docs', 'https://docs.nestjs.com'),
       () => this.db.pingCheck('database'),
     ]);
   }
