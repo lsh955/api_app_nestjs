7c6520bc0dff3147bbc29996b027e9012c7eca7d
이승환 <30866509+lsh955@users.noreply.github.com>
2022-10-24 14:49:46 +0900

    db ping check timeout 설정

--- src/health-check/health-check.controller.ts
+++ src/health-check/health-check.controller.ts
@@ -20,7 +20,7 @@ export class HealthCheckController {
     return this.health.check([
       // pingCheck 함수를 이용하여 제공하는 다른 서버가 잘 동작하고 있는지 확인
       () => this.http.pingCheck('google', 'https://google.com', {timeout: 800}),
-      () => this.db.pingCheck('database'),
+      () => this.db.pingCheck('database', {timeout: 1500}),
     ]);
   }
 }
