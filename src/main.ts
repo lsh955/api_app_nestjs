7d44d0ea99cb7325ff462f97d03b6b45f8ef5438
이승환 <30866509+lsh955@users.noreply.github.com>
2022-10-26 13:01:08 +0900

    makeOrmConfig 함수추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import {NestFactory} from '@nestjs/core';
 import {ValidationPipe} from '@nestjs/common';
+import * as fs from 'fs';
 
 import {AppModule} from './app.module';
+import {ConfigService} from './config/config.service';
 
 async function bootstrap() {
+  await makeOrmConfig();
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
@@ -11,6 +15,17 @@ async function bootstrap() {
     }),
   );
   await app.listen(3000);
+}
+
+async function makeOrmConfig() {
+  const configService = new ConfigService(process.env);
+  const typeormConfig = configService.getTypeOrmConfig();
+
+  if (fs.existsSync('ormconfig.json')) {
+    fs.unlinkSync('ormconfig.json');
+  }
+
+  fs.writeFileSync('ormconfig.json', JSON.stringify(typeormConfig, null, 2));
 }
 
 bootstrap();
